657a3bd6d53dc2cf1574fa0f9039926394f24803
DDecuypere <ddcdecuypere@gmail.com>
2024-11-08 13:57:47 +0100

    default react code cleanup, manifest customization

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
+import './css/reset.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,5 +16,5 @@ if (root){
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint.
 reportWebVitals();
