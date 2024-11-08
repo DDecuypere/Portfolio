fe89da6f31246dbb148294fbff2b9973cd706dd5
DDecuypere <ddcdecuypere@gmail.com>
2024-11-08 16:25:52 +0100

    add base styling of menu and base layout

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -3,7 +3,13 @@ import './../css/components/menu.css'
 
 function Menu() {
     return (
-        <section id='menu'></section>
+        <section id='menu'>
+            <ul>
+                <li><a href="">Start</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Games</a></li>
+            </ul>
+        </section>
     )
 }
 
